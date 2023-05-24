68e335566767f2279f2c1b602d4d178de2b7c935
Sergey Savrukhin <savra.sv@yandex.ru>
2023-05-24 19:08:03 +0300

    Initial commit

---
+++ src/main/kotlin/com/hvdbs/com/dropmytwitter/DropMyTwitterApplication.kt
@@ -0,0 +1,11 @@
+package com.hvdbs.com.dropmytwitter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DropMyTwitterApplication
+
+fun main(args: Array<String>) {
+	runApplication<DropMyTwitterApplication>(*args)
+}
---
+++ src/test/kotlin/com/hvdbs/com/dropmytwitter/DropMyTwitterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hvdbs.com.dropmytwitter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DropMyTwitterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
